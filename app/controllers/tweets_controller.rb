540876f69ea3c777139584e089a69f446495e61d
InoueTakayuki <takayuki.inoue527@gmail.com>
2018-11-29 16:51:55 +0900

    new_form

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,4 +2,13 @@ class TweetsController < ApplicationController
   def index
     @tweets = Tweet.all
   end
+
+  def new
+  end
+
+  def create
+    tweet = Tweet.params(id)
+    tweet.create
+  end
+
 end
